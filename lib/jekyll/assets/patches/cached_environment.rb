a79c980665e1de8b01d9201c42fe325b83dccc26
Thilo-Alexander Ginkel <tg@tgbyte.de>
2021-12-26 20:54:00 +0100

    Ruby 3 compatibility (fix kwargs)

--- lib/jekyll/assets/patches/cached_environment.rb
+++ lib/jekyll/assets/patches/cached_environment.rb
@@ -33,8 +33,8 @@ module Jekyll
         # @return [Sprockets::Asset]
         # --
         %i(find_asset find_asset!).each do |v|
-          define_method v do |*a|
-            super(*a).tap do |m|
+          define_method v do |*a, **kwargs|
+            super(*a, **kwargs).tap do |m|
               m&.environment = self
             end
           end
